caf594658209829e78d012f9e2b69605c0fe9cd2
EXLsunshine <exlsunshine@gmail.com>
2015-09-20 10:21:19 +0800

    Reverse word in a sting.(Completed)

--- 151.cpp
+++ 151.cpp
@@ -2,61 +2,77 @@
 #include <string>
 using namespace std;
 
-class Solution {
+class Solution 
+{
 public:
-	void reverseWords(string &s)
+    void reverseWords(string &s)
     {
-    	int i, j;
-    	for (i = 0; i < s.length(); )
-    	{
-    		if (s[i] == ' ')
-    			continue;
+        int i = 0, j = 0;
+        while (i < s.length() && j < s.length())
+        {
+            while (s[i] == ' ' && i < s.length())
+                i++;
 
-    		for (j = i + 1; j < s.length(); ++j)
-    		{
-    			if (s[j] == ' ')
-    				break;
-    		}
+            j = i;
+            while (s[j + 1] != ' ' && (j + 1) < s.length())
+                j++;
 
-    		int len = j - i;
-    		for (int k = i; k < len / 2; ++k)
-    		{
-    			char c = s[k];
-    			s[i] = s[j - 1 - k];
-    			s[j - 1 - k] = c;
-    		}
+            if (i > s.length() || j > s.length())
+                break;
 
-    		i = j + 1;
+            reverseAword(s, i, j);
 
-			cout << s << endl;
- 	    }
+            i = j + 1;
+        }
 
- 	    
- 	    for (i = 0; i < s.length() / 2; ++i)
- 	    {
- 	    	char c = s[i];
- 	    	s[i] = s[s.length() - 1 - i];
- 	    	s[s.length() - 1 - i] = c;
- 	    }
+        int new_index = 0, curr_index = 0;
+        while (curr_index < s.length())
+        {
+            while (s[curr_index] == ' ')
+                curr_index++;
+
+            while (curr_index < s.length() && s[curr_index] != ' ')
+                s[new_index++] = s[curr_index++];
+
+            while (curr_index < s.length() && s[curr_index] == ' ')
+                curr_index++;
+
+
+            if (curr_index >= s.length())
+                break; 
+            else
+                s[new_index++] = ' ';
+        }
+
+        reverseAword(s, 0, new_index - 1);
+
+        s.erase(new_index, s.length() - new_index + 1);
+        //for (int i = new_index; i < s.length(); ++i)
+        //    s[i] = '\0';
+    }
+
+private:
+    void reverseAword(string &str, int start, int end)
+    {
+        int cnt = end - start;
+        for (int i = 0; i <= cnt / 2; i++)
+        {
+            char c = str[start + i];
+            str[start + i] = str[end - i];
+            str[end - i] = c;
+        }
     }
 };
 
 int main(int argc, char const *argv[])
 {
-	Solution s;
-	string str = "the sky is blue";
+    Solution s;
+    string str = "    the    sky is   blue   ";
 
-	cout << str << endl;
-	s.reverseWords(str);
-	cout << str << endl;
-	return 0;
+    cout << str << endl;
+    s.reverseWords(str);
+    cout << str << endl;
+
+
+    return 0;
 }
-
-
-
-
-
-
-
-
-
