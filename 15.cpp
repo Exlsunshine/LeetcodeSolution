242b1bc024adf0c0f3e580778d6295bb67fa4476
EXLsunshine <exlsunshine@gmail.com>
2015-09-28 21:29:22 +0800

    3Sum(not test yet.)

--- 15.cpp
+++ 15.cpp
@@ -13,42 +13,51 @@ public:
 
     	for (int i = 0; i < nums.size(); ++i)
     	{
-    		int target = 0 - nums[i];
+            if (i != 0 && nums[i] != nums[i - 1])
+            {
+                int target = 0 - nums[i];
+        		int low = i + 1;
+        		int high = nums.size() - 1;
 
-    		int low = i + 1;
-    		int high = nums.size() - 1;
+        		while (low < high)
+        		{
+        			if (nums[low] + nums[high] == target)
+        			{
+                        v.push_back(std::vector<int>{nums[i], nums[low], nums[high]});
 
-    		while (low <= high)
-    		{
-    			if (nums[low] + nums[high] == target)
-    			{
+        				while (low < high && nums[low] == nums[low + 1])
+        					low++;
+        				while (low < high && nums[high] == nums[high - 1])
+        					high--;
 
-    				while (low < high && nums[low] == nums[low + 1])
-    					low++;
-    				while (low < high && nums[high] == nums[high - 1])
-    					high--;
-
-    				low++;
-    				high--;
-    			}
-    			else if (nums[low] + nums[high] < target)
-    				low++;
-    			else
-    				high--;
-    		}
+        				low++;
+        				high--;
+        			}
+        			else if (nums[low] + nums[high] < target)
+        				low++;
+        			else
+        				high--;
+        		}
+            }
     	}
 
+        return v;
     }
 };
 
 int main(int argc, char const *argv[])
 {
-	std::vector<int> v = {1,4,2,7,3};
+	std::vector<int> v = {-1, 0, 1, 2, -1, -4};
 
 	Solution s;
-	s.threeSum(v);
+	std::vector<std::vector<int>> vr = s.threeSum(v);
 
-	for (int i = 0; i < v.size(); ++i)
-		cout << v[i] << endl;
-	return 0;
+	for (int i = 0; i < vr.size(); ++i)
+    {
+        for (int j = 0; j < vr[i].size(); ++j)
+		  cout << vr[i][j] << " ";
+        cout << endl;
+	}
+
+    return 0;
 }
