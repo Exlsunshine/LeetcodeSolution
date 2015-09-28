ccaf09f9e0882abf4c31e064709b5f9eae1dafdb
EXLsunshine <exlsunshine@gmail.com>
2015-09-28 21:29:47 +0800

    Number of Digit One.(not test yet.)

---
+++ 233.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    int countDigitOne(int n) 
+    {
+    	int cnt = 0;
+
+    	for (int i = 1; i < n; i *= 10)
+    	{
+    		int front = n / i;
+    		int rear = n % i;
+    		int digit = front % 10;
+
+    		if (digit == 1)
+    			cnt += front / 10 * i + (b + 1);
+    		else if (digit == 0)
+    			cnt += front / 10 * i;
+    		else
+    			cnt += (front / 10 + 1) * i;
+    	}    
+
+    	return cnt;
+    }
+};
+
+int main(int argc, char const *argv[])
+{
+	return 0;
+}
