c34ff9e2991814e1ceac673c8fedc687b9ff79ff
EXLsunshine <exlsunshine@gmail.com>
2015-09-30 19:47:12 +0800

    Number of Digit One.

--- 233.cpp
+++ 233.cpp
@@ -8,14 +8,14 @@ public:
     {
     	int cnt = 0;
 
-    	for (int i = 1; i < n; i *= 10)
+    	for (int i = 1; i <= n; i *= 10)
     	{
     		int front = n / i;
     		int rear = n % i;
     		int digit = front % 10;
 
     		if (digit == 1)
-    			cnt += front / 10 * i + (b + 1);
+    			cnt += front / 10 * i + (rear + 1);
     		else if (digit == 0)
     			cnt += front / 10 * i;
     		else
@@ -28,5 +28,7 @@ public:
 
 int main(int argc, char const *argv[])
 {
+	Solution s;
+	cout << s.countDigitOne(13) << endl;
 	return 0;
 }
