4b28e1c8491e3d92473d32e21aef2464221eeeec
EXLsunshine <exlsunshine@gmail.com>
2015-10-11 09:42:42 +0800

    Search a 2D Matrix II.

---
+++ 240.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution
+{
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target)
+    {
+        int m = matrix.size();
+        int n = matrix[0].size();
+
+        int row = 0;
+        int col = n - 1;
+
+        while (row < m && col >= 0)
+        {
+            if (matrix[row][col] == target)
+                return true;
+            else if (matrix[row][col] > target)
+                col--;
+            else
+                row++;
+        }
+
+        return false;
+    }
+};
+
+int main()
+{
+    return 0;
+}
