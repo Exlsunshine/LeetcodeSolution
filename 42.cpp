0014134086bb69d18e254d4eb138a1c53f2f4f63
EXLsunshine <exlsunshine@gmail.com>
2016-01-06 12:54:05 +0800

    Trapping Rain Water.(Add an O(n) solution.)

--- 42.cpp
+++ 42.cpp
@@ -1,11 +1,44 @@
 #include <iostream>
 #include <vector>
+#include <climits>
 
 using namespace std;
 
 /*
-	Time complexity: O(n^2);
+    Time complexity: O(n);
 */
+class Solution {
+public:
+    int trap(vector<int>& height)
+    {
+        int front = 0, rear = height.size() - 1;
+        int frontMax = numeric_limits<int>::min();
+        int rearMax = numeric_limits<int>::min();
+        int water = 0;
+
+        while (front < rear)
+        {
+            if (height[front] < height[rear])
+            {
+                frontMax = max(frontMax, height[front]);
+                water += frontMax - height[front];
+                front++;
+            }
+            else
+            {
+                rearMax = max(rearMax, height[rear]);
+                water += rearMax - height[rear];
+                rear--;
+            }
+        }
+
+        return water;
+    }
+};
+
+/*
+	Time complexity: O(n^2);
+
 class Solution {
 public:
     int trap(vector<int>& height)
@@ -45,6 +78,7 @@ public:
     	return water;
     }
 };
+*/
 
 int main(int argc, char const *argv[])
 {
